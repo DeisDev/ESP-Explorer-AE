5cc1a4e571d3fab830e05b63d5fd7411d4c179b1
DeisDev <aidenanthonydeis@gmail.com>
2026-05-09 00:26:25 -0600

    feat: add configurable multi-copy formatting

--- src/GUI/Widgets/FormatUtils.cpp
+++ src/GUI/Widgets/FormatUtils.cpp
@@ -4,6 +4,21 @@
 
 namespace ESPExplorerAE::FormatUtils
 {
+    namespace
+    {
+        void AppendQuoted(std::string& text, const std::string& value)
+        {
+            text.push_back('"');
+            for (const char ch : value) {
+                if (ch == '"') {
+                    text.push_back('"');
+                }
+                text.push_back(ch);
+            }
+            text.push_back('"');
+        }
+    }
+
     std::string FormID(std::uint32_t formID)
     {
         char buffer[16]{};
@@ -11,16 +26,37 @@ namespace ESPExplorerAE::FormatUtils
         return buffer;
     }
 
-    std::string ParenthesizedList(std::span<const std::string> values)
+    std::string MultiCopyList(std::span<const std::string> values, MultiCopyFormat format)
     {
         std::string text{};
         for (std::size_t i = 0; i < values.size(); ++i) {
             if (i > 0) {
-                text += ", ";
+                switch (format) {
+                case MultiCopyFormat::Lines:
+                    text.push_back('\n');
+                    break;
+                case MultiCopyFormat::CommaSeparated:
+                case MultiCopyFormat::Parenthesized:
+                case MultiCopyFormat::QuotedCommaSeparated:
+                    text += ", ";
+                    break;
+                }
             }
-            text += "(";
-            text += values[i];
-            text += ")";
+
+            switch (format) {
+            case MultiCopyFormat::Lines:
+            case MultiCopyFormat::CommaSeparated:
+                text += values[i];
+                break;
+            case MultiCopyFormat::Parenthesized:
+                text += "(";
+                text += values[i];
+                text += ")";
+                break;
+            case MultiCopyFormat::QuotedCommaSeparated:
+                AppendQuoted(text, values[i]);
+                break;
+            }
         }
         return text;
     }
